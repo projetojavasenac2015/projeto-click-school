870b5d07850d03575a8ff58f16079703685cdf30
Rafael Lessa <ralf260@gmail.com>
2015-09-16 12:55:52 -0300

    teste commit

---
+++ src/br/com/clickschool/modelo/teste.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.clickschool.modelo;
+
+/**
+ * @author Rafael Lessa
+ *
+ */
+public class teste {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Imprimindo qualquer coisa!");
+	}
+
+}
